18bb9fdb0809da4452d41860b365797950d35f8c
IgorDGomes <igordgomes@gmail.com>
2025-01-17 23:57:57 +0000

    Add env file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import cors from "cors";
 import express from "express";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 1436
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -36,18 +40,18 @@ app.post("/", (req: Request, res: Response) => {
                 case undefined:
                     responseData[attr] = "";
                     break;
-            
+
                 default:
                     break;
             }
         })
-        
+
         if (responseData.race.trim().toLowerCase() == "half") {
             let randomHalfMainRace = Math.floor(Math.random() * (options.race.length - 1));
             const halfMainRace = options.race[randomHalfMainRace];
             let randomHalfSecondRace = Math.floor(Math.random() * (options.race.length - 1));
             const halfSecondRace = options.race[randomHalfSecondRace];
-            
+
             if (randomHalfMainRace === randomHalfSecondRace) {
                 console.log("Better luck next time!");
                 responseData.race = halfMainRace;
@@ -56,7 +60,7 @@ app.post("/", (req: Request, res: Response) => {
                 responseData.race = combinedRace;
             }
         }
-        
+
         if (responseData.classSelection.trim().toLowerCase() == "more than 1 class") {
             let randomDice = Math.floor(Math.random() * 6 + 1);
             let randomNegativeDice = Math.floor(Math.random() * 7 + 1);
@@ -74,11 +78,11 @@ app.post("/", (req: Request, res: Response) => {
                 const multiClassType = options.classSelection[randomFirstClass] + ' and ' + options.classSelection[randomSecondClass];
                 responseData.classSelection = multiClassType;
             }
-            
+
             if (dice === -3) {
                 responseData.classSelection = 'Your character have 2 classes of your choice';
             }
-            
+
             // Condition to 3 classes
             if (dice >= -5) {
 
@@ -97,23 +101,23 @@ app.post("/", (req: Request, res: Response) => {
                 if (randomClasses.length === 2) {
                     responseData.classSelection = options.classSelection[randomClasses[0]] + ' and ' + options.classSelection[randomClasses[1]];
                 }
-                
+
                 responseData.classSelection = options.classSelection[randomClasses[0]] + ', ' + options.classSelection[randomClasses[1]] + ' and ' + options.classSelection[randomClasses[2]];
-                
+
             }
-            
+
             if (dice === -6) {
                 const luck = Math.floor(Math.random() * 2);
                 const randomNumber = Math.floor(Math.random() * 1000000);
-                
+
                 if (luck === 1) {
                     if (randomNumber === 1436) {
                         responseData.classSelection = 'Your character have the ability to have all the classes';
                     }
-                    
+
                     responseData.classSelection = 'Your character have 3 classes of your choice';
                 }
-                
+
                 responseData.classSelection = 'Your character have 2 classes of your choice';
             }
         }
@@ -121,7 +125,7 @@ app.post("/", (req: Request, res: Response) => {
         return res.status(201).json(responseData);
     } catch (error) {
         console.error("An error occurred: ", error);
-        
+
         return res.status(500).json({ error: `An error occurred on the server: ${error}` });
     }
 });
